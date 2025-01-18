537f3b28b90125b7add0172046802a6e9b9583c1
nurcan <nurcannafii@outlook.com>
2025-01-18 21:59:14 +0400

    service and controller tests created

--- src/test/java/az/edu/turing/usermanager/common/TestConstants.java
+++ src/test/java/az/edu/turing/usermanager/common/TestConstants.java
@@ -2,6 +2,8 @@ package az.edu.turing.usermanager.common;
 
 import az.edu.turing.usermanager.domain.entity.UserEntity;
 import az.edu.turing.usermanager.model.dto.UserDto;
+import az.edu.turing.usermanager.model.dto.request.CreateUserRequest;
+import az.edu.turing.usermanager.model.dto.request.UpdateUserRequest;
 import az.edu.turing.usermanager.model.enums.UserStatus;
 
 public interface TestConstants {
@@ -9,6 +11,9 @@ public interface TestConstants {
     Long ID = 1L;
     String USERNAME = "admin@turing.com";
     String PASSWORD = "Admin123!";
+
+    CreateUserRequest CREATE_USER_REQUEST = new CreateUserRequest(USERNAME, PASSWORD, PASSWORD);
+    UpdateUserRequest UPDATE_USER_REQUEST = new UpdateUserRequest(USERNAME, PASSWORD);
 
     UserEntity USER_ENTITY = UserEntity.builder()
             .id(ID)
--- src/test/java/az/edu/turing/usermanager/controller/UserControllerTest.java
+++ src/test/java/az/edu/turing/usermanager/controller/UserControllerTest.java
@@ -1,110 +1,102 @@
 package az.edu.turing.usermanager.controller;
 
 import az.edu.turing.usermanager.contoller.UserController;
-import az.edu.turing.usermanager.domain.entity.UserEntity;
-import az.edu.turing.usermanager.model.dto.UserDto;
-import az.edu.turing.usermanager.model.dto.request.CreateUserRequest;
-import az.edu.turing.usermanager.model.dto.request.UpdateUserRequest;
 import az.edu.turing.usermanager.model.enums.UserStatus;
 import az.edu.turing.usermanager.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Set;
 
 import static az.edu.turing.usermanager.common.TestConstants.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(UserController.class)
 class UserControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
     private UserService userService;
 
-    @InjectMocks
-    private UserController userController;
-
-    private static final CreateUserRequest CREATE_USER_REQUEST = new CreateUserRequest(USERNAME, PASSWORD, PASSWORD);
-    private static final UpdateUserRequest UPDATE_USER_REQUEST = new UpdateUserRequest(USERNAME, PASSWORD);
-
     @Test
-    void getAll_Should_Return_Success() {
+    void getAll_Should_Return_Success() throws Exception {
         when(userService.findAll()).thenReturn(Set.of(USER_DTO));
-        ResponseEntity<Set<UserDto>> users = userController.getAll();
 
-        assertNotNull(users);
-        assertEquals(200, users.getStatusCodeValue());
-        assertEquals(Set.of(USER_DTO), users.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Set.of(USER_DTO))));
 
         verify(userService, times(1)).findAll();
     }
 
     @Test
-    void create_Should_Return_Success() {
+    void create_Should_Return_Success() throws Exception {
         when(userService.create(CREATE_USER_REQUEST)).thenReturn(USER_DTO);
 
-        ResponseEntity<UserDto> users = userController.create(CREATE_USER_REQUEST);
-
-        assertNotNull(users);
-        assertEquals(201, users.getStatusCodeValue());
-        assertEquals(USER_DTO, users.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(CREATE_USER_REQUEST)))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_DTO)));
 
         verify(userService, times(1)).create(CREATE_USER_REQUEST);
     }
 
     @Test
-    void getByUsername_Should_Return_Success() {
+    void getByUsername_Should_Return_Success() throws Exception {
         when(userService.findByUsername(USERNAME)).thenReturn(USER_DTO);
 
-        ResponseEntity<UserDto> users = userController.getByUsername(USERNAME);
-
-        assertNotNull(users);
-        assertEquals(200, users.getStatusCodeValue());
-        assertEquals(USER_DTO, users.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/users/{username}", USERNAME))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_DTO)));
 
         verify(userService, times(1)).findByUsername(USERNAME);
     }
 
     @Test
-    void update_Should_Return_Success() {
+    void update_Should_Return_Success() throws Exception {
         when(userService.update(ID, UPDATE_USER_REQUEST)).thenReturn(USER_DTO);
 
-        ResponseEntity<UserDto> users = userController.update(ID, UPDATE_USER_REQUEST);
-
-        assertNotNull(users);
-        assertEquals(200, users.getStatusCodeValue());
-        assertEquals(USER_DTO, users.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/users/{id}", ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(UPDATE_USER_REQUEST)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_DTO)));
 
         verify(userService, times(1)).update(ID, UPDATE_USER_REQUEST);
     }
 
     @Test
-    void updateStatus_Should_Return_Success() {
+    void updateStatus_Should_Return_Success() throws Exception {
         when(userService.updateStatus(ID, UserStatus.INACTIVE)).thenReturn(USER_DTO);
 
-        ResponseEntity<UserDto> users = userController.updateStatus(ID, UserStatus.INACTIVE);
-
-        assertNotNull(users);
-        assertEquals(200, users.getStatusCodeValue());
-        assertEquals(USER_DTO, users.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.patch("/api/v1/users/{id}", ID)
+                        .param("status", UserStatus.INACTIVE.name()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_DTO)));
 
         verify(userService, times(1)).updateStatus(ID, UserStatus.INACTIVE);
     }
 
     @Test
-    void delete_Should_Return_Success() {
+    void delete_Should_Return_Success() throws Exception {
         doNothing().when(userService).deleteById(ID);
 
-        ResponseEntity<UserDto> users = userController.delete(ID);
-
-        assertNotNull(users);
-        assertEquals(204, users.getStatusCodeValue());
-        assertNull(users.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/users/{id}", ID))
+                .andExpect(status().isNoContent());
 
         verify(userService, times(1)).deleteById(ID);
     }
